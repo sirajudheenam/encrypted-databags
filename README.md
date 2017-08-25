cec3db46d80892247bb1f5665403b375a739a8f6
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2017-08-25 12:37:21 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# encrypted-databag
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: encrypted-databag
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
