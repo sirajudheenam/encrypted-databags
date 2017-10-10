0a6f6417063629d357a871d1a00c53c80aa08e2c
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2017-10-10 13:23:44 +0200

    data bag path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-#Chef::Config[:data_bag_path] = '/tmp/databags'
+Chef::Config[:data_bag_path] = '/root/databags'
 #Chef::Config[:encrypted_data_bag_secret] = '/tmp/databags/secret'
 #Chef::Config[:solo_legacy_mode]=true
